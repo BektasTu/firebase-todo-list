e9ae84c3b96015abb848419bd08f53f5f82ccaf5
Tunay Bektas <what05@web.de>
2022-12-14 19:36:12 +0100

    Create Data added the function to create new todos and update them

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,44 @@ import React, { useState, useEffect } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import ToDo from "./ToDo";
 import { db } from "./firebase";
-import { query, collection, onSnapshot } from "firebase/firestore";
+import {
+  query,
+  collection,
+  onSnapshot,
+  updateDoc,
+  doc,
+  addDoc,
+} from "firebase/firestore";
 
 const style = {
-  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ed] to-[#1cb5e0]`,
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#FFA000] to-[#FDD835]`,
   container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
   heading: `text-3xl font-bold text-transparent text-center bg-clip-text bg-gradient-to-br from-orange-700 to-yellow-400  p-2`,
   form: `flex justify-between`,
   input: `border p-2 w-full text-xl`,
-  button: `border rounded-3xl p-4 ml-2 bg-orange-400 text-slate-100`,
+  button: `border rounded-3xl p-4 ml-2 bg-amber-500 text-slate-600`,
   count: `text-center p-2`,
 };
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
 
-  //Create toDo
-  //Read toDo from Firebase
+  //Create todo
+  const createTodo = async (e) => {
+    e.preventDefault(e);
+    if (input === "") {
+      alert("Es können keine leeren felder hinzugefügt werden!");
+      return;
+    }
+    await addDoc(collection(db, "todos"), {
+      text: input,
+      completed: false,
+    });
+    setInput("");
+  };
+
+  //Read todo from Firebase
   useEffect(() => {
     const q = query(collection(db, "todos"));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -30,25 +51,41 @@ function App() {
     });
     return () => unsubscribe;
   }, []);
-  //Update toDo from Firebase
+
+  //Update todo from Firebase
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, "todos", todo.id), {
+      completed: !todo.completed,
+    });
+  };
+
   //Delete toDo from Firebase
 
   return (
     <div className={style.bg}>
       <div className={style.container}>
         <h3 className={style.heading}>Firebase ToDo</h3>
-        <form className={style.form}>
-          <input className={style.input} type="text" placeholder="Add ToDo" />
+        <form onSubmit={createTodo} className={style.form}>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className={style.input}
+            type="text"
+            placeholder="Füge eine Aufgabe hinzu..."
+          />
           <button className={style.button}>
             <AiOutlinePlus size={30} />
           </button>
         </form>
         <ul>
           {todos.map((todo, index) => (
-            <ToDo key={index} todo={todo} />
+            <ToDo key={index} todo={todo} toggleComplete={toggleComplete} />
           ))}
         </ul>
-        <p className={style.count}>Du hast noch 3 sachen zu erledigen</p>
+
+        {todos.length < 1 ? null : (
+          <p className={style.count}>{`Offene Aufgaben: ${todos.length}`}</p>
+        )}
       </div>
     </div>
   );
