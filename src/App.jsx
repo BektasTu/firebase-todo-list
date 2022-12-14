b9bff4afffefed941f2b94b8861342735dedbe6b
Tunay Bektas <what05@web.de>
2022-12-14 16:21:46 +0100

    Add CSS added styling to app.jsx and todo.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import ToDo from "./ToDo";
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ed] to-[#1cb5e0]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-transparent text-center bg-clip-text bg-gradient-to-br from-orange-700 to-yellow-400  p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border rounded-3xl p-4 ml-2 bg-orange-400 text-slate-100`,
+  count: `text-center p-2`,
 };
 
 function App() {
